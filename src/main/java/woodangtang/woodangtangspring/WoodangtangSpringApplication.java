c0bd96131c1eeafaae6a78c7fac5570963861dce
Seonghyunsun <leaf1993@naver.com>
2020-11-20 22:29:24 +0900

    springBoot setting

---
+++ src/main/java/woodangtang/woodangtangspring/WoodangtangSpringApplication.java
@@ -0,0 +1,13 @@
+package woodangtang.woodangtangspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodangtangSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodangtangSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodangtang/woodangtangspring/WoodangtangSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package woodangtang.woodangtangspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodangtangSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
